90a1ad3a71f4f9f06d83335fdb0ab5098409422c
Udara Jayawardena <udarajayawardena5@gmail.com>
2025-08-27 12:22:15 +0530

    fix: code-reformat

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
+import { AppController } from './app.controller';
 import { UserController } from './user/user.controller';
+
+import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { PrismaService } from './prisma/prisma.service';
 
+import { AuthModule } from './auth/auth.module';
+
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController, UserController],
   providers: [AppService,PrismaService, UserService],
 })
