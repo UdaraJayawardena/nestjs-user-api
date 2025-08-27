29899f01d3ae42634421748a788dbd7ec85a7212
Udara Jayawardena <udarajayawardena5@gmail.com>
2025-08-27 11:58:18 +0530

    docs: swagger-updated

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('API documentation for the NestJS User API Backend')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+    
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api/docs', app, document);
+
+
   await app.listen(process.env.PORT ?? 3005);
 }
 
